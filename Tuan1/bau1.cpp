b01223aba0c3d4e22012d96cd0b474e82c2db95e
nguyentuananhuet <115002919+nguyentuananhuet@users.noreply.github.com>
2023-09-07 07:41:32 +0700

    Add files via upload

---
+++ Tuan1/bau1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	string s;
+	getline(cin,s);
+	int l = s.length();
+	for(int i = l - 1; i >=0; i--)
+	{
+		cout<<s[i];
+	}
+}
